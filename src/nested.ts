e746006e7a38bca54fda01d3b94d9d0012895f96
Olivia Bouvier <obouvier@udel.edu>
2024-09-18 11:30:06 -0400

    completed sameType

--- src/nested.ts
+++ src/nested.ts
@@ -161,7 +161,12 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    let allSameType: boolean = questions.every(
+        (question: Question): boolean => {
+            return question.type === questions[0].type;
+        },
+    );
+    return allSameType;
 }
 
 /***
