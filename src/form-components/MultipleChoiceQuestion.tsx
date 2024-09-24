239c206f67a65eb524225398ed30f56ce114808c
Olivia Bouvier <obouvier@udel.edu>
2024-09-23 22:19:02 -0400

    added MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,28 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="favoriteColors">
+                <Form.Label>Select an option</Form.Label>
+                <Form.Select
+                    value={answer}
+                    onChange={(event) => {
+                        setAnswer(event.target.value);
+                    }}
+                >
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {answer === expectedAnswer && <div>✔️</div>}
+            {answer !== expectedAnswer && <div>❌</div>}
         </div>
     );
 }
